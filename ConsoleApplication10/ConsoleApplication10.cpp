77331986774b6944764a10dda53e89de6fde8b63
Top9 <teuebatblya>
2023-06-02 21:04:17 +0300

    first

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int number, i = 1, result = 1;
+    cout << "Введите число";
+    cin >> number;
+    while (i <= number) {
+        result *= i;
+        i++;
+    }
+        cout << result;
+    
+}
